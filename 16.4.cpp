360eb50124d9e21ecb3b1adff085caf6aaf34eff
Nikita MIkhailov <nikita57979@gmail.com>
2023-10-28 18:33:11 +0300

    Добавьте файлы проекта.

---
+++ 16.4.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{	
+	struct tm buf;
+	time_t t = time(NULL);
+	localtime_s(&buf, &t);
+
+	const int N = 10, M = 5;
+	int LineForSumm = buf.tm_mday % 5;
+	int SummOfLine = 0;
+
+	int array[N][M];
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			array[i][j] = i + j;
+			cout << array[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	for (int j = 0; j < M; j++)
+	{
+		SummOfLine += array[LineForSumm][j];
+	}
+	cout << "=================================" << endl;
+	cout << "Summa " << LineForSumm << " stroki ravna: " << SummOfLine << endl;
+	cout << "=================================" << endl;
+}
+
